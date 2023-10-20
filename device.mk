9c81c8a687f5cd6b03d4a1093a4a0d75e24c5b34
johnt1989 <75329354+johnt1989@users.noreply.github.com>
2023-10-20 07:06:04 -0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -31,9 +31,8 @@ PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
     fastbootd
 
-PRODUCT_PACKAGES += \
-    libhidltransport \
-    libhwbinder
+PRODUCT_HOST_PACKAGES += \
+    libandroidicu
 
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
